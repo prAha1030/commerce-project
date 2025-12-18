610e516ecd963d2984ff755af307ed43f5e6e444
곽현민 <hunmin111@gmail.com>
2025-12-18 13:54:53 +0900

    상품 생성

--- src/commerce/Main.java
+++ src/commerce/Main.java
@@ -1,4 +1,9 @@
 package commerce;
 
 public class Main {
+    // 상품 생성
+        Product p1 = new Product("Galaxy S25", "1,200,000원", "최신 안드로이드 스마트폰", 30);
+        Product p2 = new Product("iPhone 16", "1,350,000원", "Apple의 최신 스마트폰", 30);
+        Product p3 = new Product("MacBook Pro", "2,400,000원", "M3 칩셋이 탑재된 노트북", 30);
+        Product p4 = new Product("AirPods Pro", "350,000원", "노이즈 캔슬링 무선 이어폰", 30);
 }
--- src/commerce/Product.java
+++ src/commerce/Product.java
@@ -1,4 +1,15 @@
 package commerce;
 
 public class Product {
+    String name;
+    String price;
+    String description;
+    int inventory;
+
+    public Product(String name, String price, String description, int inventory) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.inventory = inventory;
+    }
 }
