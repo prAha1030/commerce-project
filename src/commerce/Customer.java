f25bacc2f974e44588760cd5a7dcd93e0a5d3965
곽현민 <hunmin111@gmail.com>
2025-12-18 14:39:04 +0900

    고객 생성 & 캡슐화

---
+++ src/commerce/Customer.java
@@ -0,0 +1,14 @@
+package commerce;
+
+public class Customer {
+    // 이름, 이메일, 등급
+    private String name;
+    private String email;
+    private String grade;
+
+    public Customer(String name, String email, String grade) {
+        this.name = name;
+        this.email = email;
+        this.grade = grade;
+    }
+}
--- src/commerce/Main.java
+++ src/commerce/Main.java
@@ -3,15 +3,17 @@ package commerce;
 public class Main {
     public static void main(String[] args) {
         CommerceSystem cs = new CommerceSystem();
+        // 고객 정보
+        Customer customer1 = new Customer("ez", "a1b2c3@gmail.com", "Bronze");
         // 전자제품 상품 생성
-        cs.electronics.products.add(new Product("Galaxy S25", "1,200,000원", "최신 안드로이드 스마트폰", 30));
-        cs.electronics.products.add(new Product("iPhone 16", "1,350,000원", "Apple의 최신 스마트폰", 30));
-        cs.electronics.products.add(new Product("MacBook Pro", "2,400,000원", "M3 칩셋이 탑재된 노트북", 30));
-        cs.electronics.products.add(new Product("AirPods Pro", "350,000원", "노이즈 캔슬링 무선 이어폰", 30));
+        cs.getElectronics().addProduct(new Product("Galaxy S25", "1,200,000원", "최신 안드로이드 스마트폰", 30));
+        cs.getElectronics().addProduct(new Product("iPhone 16", "1,350,000원", "Apple의 최신 스마트폰", 30));
+        cs.getElectronics().addProduct(new Product("MacBook Pro", "2,400,000원", "M3 칩셋이 탑재된 노트북", 30));
+        cs.getElectronics().addProduct(new Product("AirPods Pro", "350,000원", "노이즈 캔슬링 무선 이어폰", 30));
         // 의류 상품 생성 테스트
-        cs.clothes.products.add(new Product("발렌시아가", "1,200,000원", "멋진 옷", 30));
+        cs.getClothes().addProduct(new Product("발렌시아가", "1,200,000원", "멋진 옷", 30));
         // 식품 상품 생성 테스트
-        cs.foods.products.add(new Product("비비고 왕만두", "100,000원", "맛있는 왕만두", 30));
+        cs.getFoods().addProduct(new Product("비비고 왕만두", "100,000원", "맛있는 왕만두", 30));
 
         // 콘솔
         cs.start();
